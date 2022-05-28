f52584e9cfd09128fe79bda98922aa4b077315a6
HashAge <aleshka.eagle@bk.ru>
2022-05-28 19:58:32 +0300

    First three ciphers plus autohiding textboxes for cesaer and vigenere ciphers

---
+++ cryptez2/Ciphers.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptez2
+{
+    internal class Ciphers
+    {
+        public string cesaer_method(string plain, int shift)
+        {
+            plain.ToLower();
+            char[] chr = plain.ToCharArray();
+            for (int i = 0; i < chr.Length; i++)
+            {
+                if (chr[i] + shift > 122)
+                {
+                    int div = chr[i] + shift - 123;
+                    chr[i] = Convert.ToChar(97 + div);
+                }
+                else chr[i] = Convert.ToChar(chr[i] + shift);
+            }
+            plain = new string(chr);
+            return plain;
+        }
+        public string vigenere_method(string plain, string shift)
+        {
+            plain.Trim();
+            char[] mes_char = plain.ToCharArray();
+            char[] code_char = shift.ToCharArray();
+            char[] code_map = new char[plain.Length];
+            int[] int_code_map = new int[plain.Length];
+            for (int x = 0, i = 0; x < plain.Length; x++)
+            {
+                if (i < shift.Length)
+                {
+                    code_map[x] = code_char[i];
+                    i++;
+                }
+                else
+                {
+                    i = 0;
+                    code_map[x] += code_char[i];
+                    i++;
+                }
+                int_code_map[x] = Convert.ToInt32(code_map[x] - 97);
+            }
+            for (int i = 0; i < plain.Length; i++)
+            {
+                if (Convert.ToChar(mes_char[i] + int_code_map[i]) > 122)
+                {
+                    int div = mes_char[i] + int_code_map[i] - 123;
+                    mes_char[i] = Convert.ToChar(97 + div);
+                }
+                else mes_char[i] = Convert.ToChar(mes_char[i] + int_code_map[i]);
+
+            }
+            plain = new string(mes_char);
+            return plain;
+        }
+        public string base32_method(string plain)
+        {
+            byte[] mes_byte = Encoding.ASCII.GetBytes(plain);
+            const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+            string output = "";
+            for (int bitIndex = 0; bitIndex < mes_byte.Length * 8; bitIndex += 5)
+            {
+                int dualbyte = mes_byte[bitIndex / 8] << 8;
+                if (bitIndex / 8 + 1 < mes_byte.Length)
+                    dualbyte |= mes_byte[bitIndex / 8 + 1];
+                dualbyte = 0x1f & (dualbyte >> (16 - bitIndex % 8 - 5));
+                output += alphabet[dualbyte];
+            }
+            return output;
+        }
+     /*   public string atbash_method(string plain)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] chr_arr = plain.ToCharArray();
+            for (int i = 0; i < plain.Length; i++) chr_arr[i] = (char)(int)(alphabet[alphabet.Length] - chr_arr[i]);
+            plain = new string(chr_arr);
+            return plain;
+        }*/
+    }
+}
---
+++ cryptez2/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cryptez2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn.Click += btn_click;
+            ces_btn.Click += switch_click;
+            vig_btn.Click += switch_click;
+            bas_btn.Click += switch_click;
+        }
+        private void switch_click(object sender, RoutedEventArgs e)
+        {        
+            if (ces_btn.IsChecked == true)
+            {
+                ces_tbx.Visibility = Visibility.Visible;
+                vig_tbx.Visibility = Visibility.Hidden;
+            }
+            if (vig_btn.IsChecked == true)
+            {
+                vig_tbx.Visibility = Visibility.Visible;
+                ces_tbx.Visibility = Visibility.Hidden;
+            }
+            if(bas_btn.IsChecked == true)
+            {
+                vig_tbx.Visibility = Visibility.Hidden;
+                ces_tbx.Visibility = Visibility.Hidden;
+            }
+
+        }
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            Ciphers cip = new Ciphers(); string plain = pln_tbx.Text;
+            string vigshift = vig_tbx.Text;
+            string cesshift = ces_tbx.Text;
+            if (ces_btn.IsChecked == true) {
+                if (ces_tbx.Text == "" || Int32.TryParse(cesshift, out int a) == false) MessageBox.Show("Enter a valid value!");
+                else pln_tbx.Text = cip.cesaer_method(plain,Int32.Parse(cesshift)); }
+            if(vig_btn.IsChecked == true)
+            {
+                if (vig_tbx.Text == "") MessageBox.Show("Enter something!");
+                else pln_tbx.Text = cip.vigenere_method(plain.Trim(),vigshift.Trim());
+            }
+            if (bas_btn.IsChecked == true) pln_tbx.Text = cip.base32_method(plain);
+            if (atb_btn.IsChecked == true) MessageBox.Show("In progress");
+            //   if (vig_btn.IsChecked == true) pln_tbx.Text = cip.vigenere_method();
+            //   if (bas_btn.IsChecked == true) pln_tbx.Text = cip.base32_method();
+        } 
+
+    }
+}
